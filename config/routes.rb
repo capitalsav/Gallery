19d5b04ded884150f71d62b5fae75ffa92cc2e03
Anton <capitalsav@gmail.com>
2017-11-23 16:47:10 +0200

    show images by category

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :categories, only: [:new]
   resources :images
+  match '/categories/:name', to: 'categories#show_images', via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
