c7ccd9111b9ef0ad03fc699cc9cf3c19e22989b5
Anton <capitalsav@gmail.com>
2017-11-30 14:42:54 +0200

    issue with default assets show

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
-    @carousel_images = Image.order("RANDOM()").limit(25)
+    # @carousel_images = Image.order("RANDOM()").limit(5)
+    @carousel_images = Image.order("RANDOM()").limit(0)
+    unless @carousel_images.present?
+      image_path = "default.jpg"
+      image = Image.new
+      image.image = image_path
+      @carousel_images = [image]
+    end
   end
 end
