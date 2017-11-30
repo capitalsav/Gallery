26fa2f9e109dd43818d54c8faebce6152ed4272d
Anton <capitalsav@gmail.com>
2017-11-30 18:06:52 +0200

    resolve static pages controller nil issue

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-    # @carousel_images = Image.order("RANDOM()").limit(5)
-    @carousel_images = Image.order("RANDOM()").limit(0)
-    unless @carousel_images.present?
-      image_path = "default.jpg"
-      image = Image.new
-      image.image = image_path
-      @carousel_images = [image]
+    images_array = Image.order("RANDOM()").limit(5)
+    uploaded = true
+    if images_array.present?
+      @carousel_images = {:images_uploaded => uploaded, :images => images_array}
+    else
+      uploaded = false
+      static_image = "/assets/default.jpg"
+      @carousel_images = {:images_uploaded => uploaded, :images => static_image}
     end
   end
 end
