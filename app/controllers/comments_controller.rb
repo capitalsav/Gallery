6ce2842c4626eb5c996e062b2f01fbf99f4cdc9d
Anton <capitalsav@gmail.com>
2017-12-07 16:02:52 +0200

    show comments and update after post new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.create!(image_id: params[:image_id], text: params[:comment][:text])
-    # if @comment.save
-    #   puts "============save"
-    #   # flash[:success] = "Micropost created!"
-    #   # redirect_to root_url
-    # else
-    #   puts "=========notSave"
-    #   # @feed_items = []
-    #   # render 'static_pages/home'
-    # end
+    image = Image.find(params[:image_id])
+    @comments = image.comments.all
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+    end
   end
 end
