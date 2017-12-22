7244787196a1563423d92ce48967746b04a12320
Anton <capitalsav@gmail.com>
2017-12-22 17:30:49 +0200

    generate active admin resources categories, images, likes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :image
   validates :user_id, presence: true
   validates :image_id, presence: true
-  has_many :comments_categories, :through => :images, :source => :categories
+
 end
