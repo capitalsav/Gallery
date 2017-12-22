c7082c9b51f28fd0e7601693e7cb960502a68810
Anton <capitalsav@gmail.com>
2017-12-22 09:57:55 +0200

    make links to top 5 categories and All categories on header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :get_top_categories
+  def get_top_categories
+    @top_categories = Category.
+        left_outer_joins(:images).distinct.select('categories.*, COUNT(images.*) AS images_count').
+        left_outer_joins(:liked_categoties).distinct.select('categories.*, COUNT(likes.*) AS likes_count').
+        left_outer_joins(:commented_categories).distinct.select('categories.*, COUNT(comments.*) AS comments_count').
+        group('categories.id').order("images_count DESC").order("likes_count DESC").order("comments_count DESC").limit(5)
+  end
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :avatar])
