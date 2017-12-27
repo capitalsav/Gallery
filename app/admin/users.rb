2bd6962fb29476c6b43946bd360c84f3d6a0e3bc
Anton <capitalsav@gmail.com>
2017-12-27 18:53:45 +0200

    show user model in active admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -11,5 +11,25 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
   filter :created_at
+
+  index  do
+    column :email
+    column :name
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :current_sign_in_ip
+    column :last_sign_in_ip
+    column :confirmed_at
+    column :confirmation_sent_at
+    column :unconfirmed_email
+    column :created_at
+    column :updated_at
+    column ('Avatar') do |user|
+      if user.avatar.present?
+        image_tag user.avatar.small_thumb.url
+    end
+  end
+  end
 end
