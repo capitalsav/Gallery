ada531cda6bf3ed6c34353bb6c207a9012effc79
Anton <capitalsav@gmail.com>
2017-12-28 12:56:58 +0200

    admin can create, edit and delete users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :email, :name, :password
 #
 # or
 #
@@ -11,9 +11,14 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.input :email
+    f.input :name
+    f.input :password
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
   filter :created_at
-
   index  do
     column :email
     column :name
@@ -29,7 +34,26 @@ ActiveAdmin.register User do
     column ('Avatar') do |user|
       if user.avatar.present?
         image_tag user.avatar.small_thumb.url
+      end
+    end
+    actions
+  end
+
+  controller do
+    def update
+      if params[:user][:password].blank?
+        params[:user].delete("password")
+        params[:user].delete("password_confirmation")
+      end
+      if params[:user][:email].blank?
+        params[:user].delete("email")
+        params[:user].delete("email_confirmation")
+      end
+      super
     end
   end
+  before_save do |user|
+    user.skip_confirmation!
+    user.skip_reconfirmation!
   end
 end
