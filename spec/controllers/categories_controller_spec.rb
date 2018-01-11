5b40a5e722fd47d8af6feb2ff9706105aa1ee58c
Anton <capitalsav@gmail.com>
2018-01-11 15:39:21 +0200

    make test for categories create action

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
-  describe "GET index" do
-    it "assigns @categories_with_images" do
-    end
+  before(:each) do
+    @user = create(:user)
+    sign_in @user
+  end
 
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
+  describe 'GET #new' do
+    it 'should be successful(NEW)' do
+      get :new
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('new')
+    end
+  end
+
+  describe 'POST #create' do
+    it 'creates category' do
+      expect do
+        post :create, params: {
+          category: attributes_for(:category, user_id: @user.id)
+        }
+      end.to change { Category.count }.by(1) and
+        redirect_to Category.last && have_http_status(200) and
+        render_template('show')
     end
   end
 end
