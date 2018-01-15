73c49dbf8dfab9a4a8a2bb46840463a6e3110ede
Anton <capitalsav@gmail.com>
2018-01-15 15:12:46 +0200

    make tests for likes model

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  it 'belong to User' do
+    expect(subject).to belong_to(:user).class_name('User')
+  end
+
+  it 'belong to Image' do
+    expect(subject).to belong_to(:image).class_name('Image')
+  end
+
+  it 'validates presence the image_id ' do
+    is_expected.to validate_presence_of(:image_id)
+  end
+
+  it 'validates presence the user_id ' do
+    is_expected.to validate_presence_of(:user_id)
+  end
+end
