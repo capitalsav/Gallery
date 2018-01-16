0afc8a4a2f4a3c99fa9ddf057b406914336d1b37
Anton <capitalsav@gmail.com>
2018-01-16 15:52:55 +0200

    update subscription model tests with validations

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -16,4 +16,28 @@ RSpec.describe Subscription, type: :model do
   it 'validates presence the user_id ' do
     is_expected.to validate_presence_of(:user_id)
   end
+
+  describe '#user_id' do
+    it 'should validate presence' do
+      record = Subscription.new
+      record.user_id = '' # invalid state
+      record.valid? # run validations
+      expect(record.errors[:user_id]).to include('can\'t be blank')
+      record.user_id = 1 # valid state
+      record.valid? # run validations
+      expect(record.errors[:user_id]).not_to include('can\'t be blank')
+    end
+  end
+
+  describe '#category_id' do
+    it 'should validate presence' do
+      record = Subscription.new
+      record.category_id = '' # invalid state
+      record.valid? # run validations
+      expect(record.errors[:category_id]).to include('can\'t be blank')
+      record.category_id = 1 # valid state
+      record.valid? # run validations
+      expect(record.errors[:category_id]).not_to include('can\'t be blank')
+    end
+  end
 end
