03e070b1279296dccd5fd4cb21628db275fa612b
Anton <capitalsav@gmail.com>
2018-01-16 15:40:45 +0200

    update comment model tests with validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :image
   validates :user_id, presence: true
   validates :image_id, presence: true
-
+  validates :text, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -16,4 +16,40 @@ RSpec.describe Comment, type: :model do
   it 'validates presence the user_id ' do
     is_expected.to validate_presence_of(:user_id)
   end
+
+  describe '#text' do
+    it 'should validate presence' do
+      record = Comment.new
+      record.text = '' # invalid state
+      record.valid? # run validations
+      expect(record.errors[:text]).to include('can\'t be blank')
+      record.text = 'test' # valid state
+      record.valid? # run validations
+      expect(record.errors[:text]).not_to include('can\'t be blank')
+    end
+  end
+
+  describe '#user_id' do
+    it 'should validate presence' do
+      record = Comment.new
+      record.user_id = '' # invalid state
+      record.valid? # run validations
+      expect(record.errors[:user_id]).to include('can\'t be blank')
+      record.user_id = 1 # valid state
+      record.valid? # run validations
+      expect(record.errors[:user_id]).not_to include('can\'t be blank')
+    end
+  end
+
+  describe '#image_id' do
+    it 'should validate presence' do
+      record = Comment.new
+      record.image_id = '' # invalid state
+      record.valid? # run validations
+      expect(record.errors[:image_id]).to include('can\'t be blank')
+      record.image_id = 1 # valid state
+      record.valid? # run validations
+      expect(record.errors[:image_id]).not_to include('can\'t be blank')
+    end
+  end
 end
