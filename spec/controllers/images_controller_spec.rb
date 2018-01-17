8e2aec371e0e5acb29fbeb45ad114d6bcd24971b
Anton <capitalsav@gmail.com>
2018-01-17 10:23:22 +0200

    make all tests for controller images

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe ImagesController, type: :controller do
   before(:each) do
     @user = create(:user)
     sign_in @user
+    @category = create(:category, user_id: @user.id)
   end
 
   describe 'GET #new' do
     it 'render new template' do
-      category = create(:category, user_id: @user.id)
-      get(:new, params: { id: category.id })
+      get(:new, params: { id: @category.id })
       expect(response).to have_http_status(200)
       expect(response).to render_template('new')
     end
@@ -17,17 +17,25 @@ RSpec.describe ImagesController, type: :controller do
 
   describe 'POST #create' do
     it 'creates image' do
-      category = create(:category, user_id: @user.id)
-      puts @user.id
       expect do
         post :create, params: { image: attributes_for(:image, user_id: @user.id),
-                                category_id: category.id,
+                                category_id: @category.id,
                                 user_id: @user.id,
-                                id: category.id,
-        }
+                                id: @category.id }
       end.to change { Image.count }.by(1) and
-          redirect_to single_category_image_path(category.slug, Image.last.id) && have_http_status(200) and
-          render_template('show')
+        redirect_to single_category_image_path(@category.slug, Image.last.id) &&
+        have_http_status(200) &&
+        render_template('show')
+    end
+  end
+
+  describe 'SHOW' do
+    it 'assigns the requested image to @image' do
+      image = create(:image, user_id: @user.id, category_id: @category.id)
+      get :show, params: { id: @category.id, image_id: image.id }
+      expect(assigns(:image)).to eq(image)
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:show)
     end
   end
 end
