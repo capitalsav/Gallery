46b03fb51dc1f45ad904eb80be6b09d5801d25bb
Anton <capitalsav@gmail.com>
2018-01-19 11:33:14 +0200

    make tests for sign_in page capybara selenium

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'UserSignin', type: :feature do
+  before(:each) do
+    @user = create(:user)
+    visit '/users/sign_in'
+  end
+
+  it 'should allow a registered user to sign in', js: true do
+    fill_in 'sing-in-email-input', with: @user.email
+    fill_in 'sign-in-password-input', with: @user.password
+    click_button 'btn-sign-in'
+    expect(current_path) == root_path
+  end
+
+  it 'should click on sign-up route to new_registration_path', js: true do
+    click_on 'shared-sing-up-link'
+    expect(current_path) == new_user_registration_path
+  end
+
+  it 'should click on forgot password route to new_password_path', js: true do
+    click_on 'shared-forgot-pwd-link'
+    expect(current_path) == new_user_password_path
+  end
+
+  it 'click on don\'t receive conf route to new_confirmation_path', js: true do
+    click_on 'shared-confirmation-link'
+    expect(current_path) == new_user_confirmation_path
+  end
+end
