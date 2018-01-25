e41beb5912c7ecc7eb92787fbb485aadcec30509
Anton <capitalsav@gmail.com>
2018-01-25 13:58:22 +0200

    make Reset password page front

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,25 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" unless devise_error_messages?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:p, msg, style: 'text-align: center;') }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="devise-errors" id="error_explanation">
+      <h5>
+        <p class="text-center">#{sentence}
+      </h5>
+      #{messages}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+end
