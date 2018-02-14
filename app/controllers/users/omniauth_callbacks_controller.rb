b16ee75bac14603238bcb864fee9ff87ac08c4ea
Anton <capitalsav@gmail.com>
2018-02-14 14:10:31 +0200

    refactor omniauth class with rubocop

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,10 @@
+# Controller for authentication via social networks
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    user = ::User.from_omniauth(request.env["omniauth.auth"])
-    if User.exists?(email: user.email)
-      set_flash_message(:success, :success)
-      sign_in(User.find_by(email: user.email), scope: :user)
-      redirect_to root_path
-    else
-      user.save!
-      set_flash_message(:success, :success)
-      sign_in(User.find_by(email: user.email), scope: :user)
-      redirect_to root_path
-    end
+    user = ::User.from_omniauth(request.env['omniauth.auth'])
+    user.save! unless User.exists?(email: user.email)
+    set_flash_message(:success, :success)
+    sign_in(User.find_by(email: user.email), scope: :user)
+    redirect_to root_path
   end
 end
